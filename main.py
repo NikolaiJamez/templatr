d6e3796af9322e43571190ddf6492051c58528a4
NikolaiJamez <nikolaijamez@gmail.com>
2024-09-30 21:21:43 +1300

    Customised template display

--- main.py
+++ main.py
@@ -3,25 +3,71 @@ from blivet.partitioning import align_size_for_disklabel
 from setuptools.command.saveopts import saveopts
 
 
-def main(page: ft.Page):
+def main (page: ft.Page):
+    # Page Attributes
     page.window.frameless = True
 
-    def create_template(e: ft.ControlEvent):
-        raise NotImplemented("create_template function note implemented")
+    # Functions
+    def create_template (e: ft.ControlEvent):
+        templates_row.controls.append(
+            ft.Card(
+                content = ft.Container(
+                    content = ft.Column(
+                        [
+                            ft.Row(
+                                [
+                                    ft.Text(
+                                        opacity = 0.5,
+                                        size = 8,
+                                        value = "Category",
+                                        italic = True,
+                                    ),
+                                    ft.IconButton(
+                                        height = 32,
+                                        icon = ft.icons.CLOSE,
+                                        icon_color = ft.colors.RED,
+                                        icon_size = 16,
+                                        visible = False,
+                                        width = 32)
+                                ],
+                                alignment = ft.MainAxisAlignment.SPACE_BETWEEN,
+                            ),
+                            ft.Row(
+                                [
+                                    ft.Text(
+                                        size = 16,
+                                        value = "Title Text",
+                                        weight = ft.FontWeight.W_700,
+                                    ),
 
-    def save_templates(e: ft.ControlEvent):
+                                ],
+                                alignment = ft.MainAxisAlignment.SPACE_BETWEEN,
+                            ),
+                            ft.ResponsiveRow([ft.Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae velit massa. Proin tempus placerat nunc, et dignissim odio lacinia at. Donec fermentum luctus ante nec pulvinar.")]),
+                        ]
+                    ),
+                    padding = 10,
+                ),
+                col = {"md": 5, "lg": 4}
+            )
+        )
+        templates_row.update()
+
+    def save_templates (e: ft.ControlEvent):
         raise NotImplemented("save_templates function note implemented")
 
-    def refresh_templates(e: ft.ControlEvent):
+    def refresh_templates (e: ft.ControlEvent):
         raise NotImplemented("refresh_templates function note implemented")
 
-    def reorder_templates(e: ft.ControlEvent):
+    def reorder_templates (e: ft.ControlEvent):
         raise NotImplemented("reorder_templates function note implemented")
 
-    def delete_all_templates(e: ft.ControlEvent):
+    def delete_all_templates (e: ft.ControlEvent):
         raise NotImplemented("delete_all_templates function note implemented")
 
     nav_filter = ft.TextField(label = "Search/Filter")
+    templates_row = ft.ResponsiveRow(columns = 20)
+
     save_button = ft.IconButton(
         icon = ft.icons.SAVE,
         icon_color = ft.colors.GREEN,
@@ -56,7 +102,6 @@ def main(page: ft.Page):
         ],
     )
 
-    templates_row = ft.ResponsiveRow()
 
     page.add(
         ft.Divider(opacity = 0),
