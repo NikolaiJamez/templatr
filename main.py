92dee46e39cdb4deff4adf9084f5a3aef1519d6a
NikolaiJamez <nikolaijamez@gmail.com>
2024-09-30 19:39:20 +1300

    Added Navbar Buttons for common actions

--- main.py
+++ main.py
@@ -5,12 +5,39 @@ from setuptools.command.saveopts import saveopts
 def main(page: ft.Page):
     page.window.frameless = True
 
+    def save_templates():
+        pass
+
+    def refresh_templates():
+        pass
+
+    def reorder_templates():
+        pass
+
+    def delete_all_templates():
+        pass
 
     nav_filter = ft.TextField(label = "Search/Filter")
-    save_button = ft.IconButton(icon = ft.icons.ADD)
-    refresh_button = ft.IconButton(icon = ft.icons.ADD)
-    reorder_button = ft.IconButton(icon = ft.icons.ADD)
-    delete_button = ft.IconButton(icon = ft.icons.ADD)
+    save_button = ft.IconButton(
+        icon = ft.icons.SAVE,
+        icon_color = ft.colors.GREEN,
+        tooltip = "Save",
+        on_click = save_templates)
+    refresh_button = ft.IconButton(
+        icon = ft.icons.REFRESH,
+        tooltip = "Refresh",
+        on_click = refresh_templates)
+    reorder_button = ft.IconButton(
+        icon = ft.icons.SHUFFLE,
+        tooltip = "Reorder",
+        on_click = reorder_templates)
+    delete_button = ft.IconButton(
+        icon = ft.icons.DELETE,
+        icon_color = ft.colors.RED,
+        tooltip = "Delete All - WARNING",
+        on_click = delete_all_templates)
+
+
 
     page.appbar = ft.AppBar(
         leading = ft.Icon(ft.icons.COPY_ALL),
