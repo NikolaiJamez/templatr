c1556888862d48906489e8d20f2adc25e67cb767
NikolaiJamez <nikolaijamez@gmail.com>
2024-09-30 19:17:15 +1300

    Added Search/Filter field

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import flet as ft
+from setuptools.command.saveopts import saveopts
+
 
 def main(page: ft.Page):
     page.window.frameless = True
+
+
+    nav_filter = ft.TextField(label = "Search/Filter")
 
     page.appbar = ft.AppBar(
         leading = ft.Icon(ft.icons.COPY_ALL),
         leading_width = 50,
         title = ft.Text("Templatr"),
         bgcolor = ft.colors.SURFACE_VARIANT,
+        actions = [
+            nav_filter,
+            ft.VerticalDivider(width = 50, opacity = 0),
+        ],
     )
 
     page.add()
